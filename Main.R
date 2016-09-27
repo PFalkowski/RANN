a649b60c54fd781207739ec775f8802294bda9f0
Piotr <piotr.falkowski.fm@gmail.com>
2016-09-27 23:12:48 +0200

    Preprocess and load data

--- Main.R
+++ Main.R
@@ -1,3 +1,14 @@
 source("Helper.R")
-Wants = c("caret")
-SetupEnvironment(requiredPackages = Wants)
+SetupEnvironment(requiredPackages = c("caret", "reshape"))
+
+# read single file
+
+data <- read.csv("../../../Downloads/mstcgl/MBANK.mst", header=TRUE)
+
+# get data bulk
+# http://stackoverflow.com/a/11433532/3922292
+# temp = list.files(path = "../../../Downloads/test", pattern="*.mst", full.names	= TRUE)
+
+# rename column names
+names(data) <- gsub("X.", "", names(data))
+names(data) <- gsub("\\.", "", names(data))
